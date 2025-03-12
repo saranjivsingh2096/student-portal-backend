9f104616d930504f22633b709b9be1aa9f8291fc
Saranjiv Singh <saranjivsingh2096@proton.me>
2025-03-12 18:58:52 +0530

    Add files via upload

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -1,8 +1,15 @@
-const { getStudentProfile, getAttendanceData, getFeeDetails } = require("../controllers/studentController");
+const {
+  getStudentProfile,
+  getAttendanceData,
+  getFeeDetails,
+  getInternalMarks,
+} = require("../controllers/studentController");
 const authenticateToken = require("../middlewares/authenticateToken");
 
 module.exports = (app) => {
   app.get("/student-profile", authenticateToken, getStudentProfile);
   app.get("/attendance-data", authenticateToken, getAttendanceData);
   app.get("/fee-details", authenticateToken, getFeeDetails);
+  app.get("/course-details", authenticateToken, getFeeDetails);
+  app.get("/internal-marks", authenticateToken, getInternalMarks);
 };
